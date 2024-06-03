f8d3bb43b09aa8f6d5940c29c63dd87b4ef10fbe
m <>
2024-06-03 11:31:25 -0400

    add skills component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
 import About from "@/components/about";
 import Hero from "@/components/hero";
 import Projects from "@/components/projects";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <main
       id="main"
-      className="h-[200vh] mt-20 scroll-mt-20"
+      className="mt-20 scroll-mt-20"
     >
       <Hero />
       <About />
       <Projects />
-      <div
-        id="contact"
-        className="bg-black"
-      >
-        <p className="text-white mt-56">hello</p>
-      </div>
+      <Skills />
     </main>
   );
 }
---
+++ components/skills.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const skills = () => {
+  return (
+    <section
+      id="skills"
+      className="flex flex-col bg-slate-200 justify-center items-center my-20"
+    >
+      <h2>skills</h2>
+      <div>
+        {/* TODO add skills */}
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, in
+        velit sint eius fugit facere asperiores quas illum eveniet repudiandae
+        cum, nisi, natus enim porro! Corrupti unde illum similique dolore?
+      </div>
+      <button>more skills</button>
+    </section>
+  );
+};
+
+export default skills;
