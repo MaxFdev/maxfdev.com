9946d966ec1a1141cc92f089b211a7e5466cd57c
m <>
2024-06-17 12:54:20 -0400

    remove scroll bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="no-scrollbar"
+    >
       <Body className={`${notoSans.variable}`}>
         <Header />
         {children}
