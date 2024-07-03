4c18684cd48bf1eedd08af9f603618a6ee4e4df2
m <>
2024-07-03 15:15:18 -0400

    fix carousel by creating a self reloading component

---
+++ components/elements/projectCarousel.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { projectItems } from "@/data";
+import Project from "./project";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+
+const projectCarousel = () => {
+  // State to store a unique key for forcing remount
+  const [componentKey, setComponentKey] = useState(0);
+
+  useEffect(() => {
+    // Function to handle window resize
+    const handleResize = () => {
+      setComponentKey((prevKey) => prevKey + 1);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // TODO make more mobile friendly
+
+  // TODO finish styling
+
+  return (
+    <Carousel
+      className="w-[76vw]"
+      opts={{ watchDrag: false, align: "start" }}
+      key={componentKey}
+    >
+      <CarouselContent className="py-4">
+        {projectItems.map((project, key) => (
+          <CarouselItem
+            className="md:basis-1/2 lg:basis-1/3"
+            key={key}
+          >
+            <Project
+              img={project.img}
+              color={project.color}
+              title={project.title}
+              content={project.content}
+              tags={project.tags}
+              link={project.link}
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
+
+export default projectCarousel;
--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import Project from "../elements/project";
-import { projectItems } from "@/data";
+import ProjectCarousel from "../elements/projectCarousel";
 import BigButton from "../elements/bigButton";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../ui/carousel";
 
 // TODO finish and fix styling
-
-// TODO force this to reload on resize
 
 const projects = () => {
   return (
@@ -21,31 +11,7 @@ const projects = () => {
       className="flex flex-col justify-center items-center scroll-mt-20 gap-[var(--gap-clamp)]"
     >
       <h2 className="text-center">Some Of My Projects.</h2>
-      {/* TODO make mobile friendly */}
-      <Carousel
-        className="w-[76vw]"
-        opts={{ watchDrag: false, align: "start" }}
-      >
-        <CarouselContent className="py-4">
-          {projectItems.map((project, key) => (
-            <CarouselItem
-              className="md:basis-1/2 lg:basis-1/3"
-              key={key}
-            >
-              <Project
-                img={project.img}
-                color={project.color}
-                title={project.title}
-                content={project.content}
-                tags={project.tags}
-                link={project.link}
-              />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+      <ProjectCarousel />
       {/* TODO style button */}
       <BigButton
         href={"https://github.com/MaxFdev"}
