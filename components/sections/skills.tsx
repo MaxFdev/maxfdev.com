4d279396e0c9a9a82982e094c7d6386094309b8f
m <meirmaxf11@gmail.com>
2024-07-04 12:03:49 -0400

    start on skills section

--- components/sections/skills.tsx
+++ components/sections/skills.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BigButton from "../elements/bigButton";
 
 // TODO finish
 
@@ -8,16 +9,22 @@ const skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col bg-slate-200 justify-center items-center my-20"
+      className="flex flex-col justify-center items-center scroll-mt-16 my-20 py-16 gap-16 bg-slate-200"
     >
-      <h2>skills</h2>
+      <h2>What I Know And Use.</h2>
       <div>
         {/* TODO add skills */}
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, in
         velit sint eius fugit facere asperiores quas illum eveniet repudiandae
         cum, nisi, natus enim porro! Corrupti unde illum similique dolore?
       </div>
-      <button>more skills</button>
+      <BigButton
+        href="https://www.linkedin.com/in/max--franklin/"
+        target="_blank"
+        className="hover:bg-blue-400"
+      >
+        More Skills - LinkedIn
+      </BigButton>
     </section>
   );
 };
