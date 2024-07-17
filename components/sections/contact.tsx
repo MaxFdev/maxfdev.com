ee38e82a313d1cc76e419b9b6f0bb7a64ee8e5e2
Max F <meirmaxf11@gmail.com>
2024-07-17 11:59:37 -0400

    start on contact section & added dialog component

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,14 +1,47 @@
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+  DialogFooter,
+} from "../ui/dialog";
 
 // TODO add styles and content
 
 const contact = () => {
   return (
-    <section id="contact" className="flex flex-col justify-center items-center">
-      <h2>contact</h2>
-      <div className="flex justify-center items-center gap-4">
-        <div>socials</div>
-        <div>other</div>
+    <section
+      id="contact"
+      className="flex flex-col justify-center items-center"
+    >
+      <h2 className="text-center">Contact me.</h2>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h3>Here are all my details.</h3>
+        <Dialog>
+          <DialogTrigger>Get details.</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle asChild>
+                <h3>Contact & Socials</h3>
+              </DialogTitle>
+              <DialogDescription asChild>
+                <p>Here are all my public accounts.</p>
+              </DialogDescription>
+            </DialogHeader>
+            {/* TODO finish adding socials */}
+            <ul>
+              <li>email</li>
+              <li>linkedin</li>
+              <li>website</li>
+              <li>something</li>
+              <li>etc</li>
+            </ul>
+          </DialogContent>
+        </Dialog>
       </div>
     </section>
   );
