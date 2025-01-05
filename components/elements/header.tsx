d0b0c64ad13ca444595145e1e165b83e9acf449b
Max F <maxfranklin@maxfdev.com>
2025-01-05 00:24:24 -0800

    update header message

--- components/elements/header.tsx
+++ components/elements/header.tsx
@@ -10,14 +10,20 @@ const header = () => {
   return (
     <header
       id="header"
-      className="fixed z-50 bg-white top-0 left-0 w-full h-16 px-[2vw] lg:px-12 flex justify-between items-center border-b border-b-blue-500/50 shadow-md"
+      className="fixed z-50 top-0 left-0 w-full border-b border-b-blue-500/50 shadow-md"
     >
-      <Link href="/#main">
-        <h1 className="font-verdana text-blue-500 font-semibold text-3xl [text-shadow:_1px_1px_0px_#000000ab]">
-          Max Franklin
-        </h1>
-      </Link>
-      <Navlist />
+      <p className="bg-black text-white text-center">
+        This site is still under development. Some parts may not be fully
+        functional.
+      </p>
+      <div className="bg-white h-16 px-[2vw] lg:px-12 flex justify-between items-center">
+        <Link href="/#main">
+          <h1 className="font-verdana text-blue-500 font-semibold text-3xl [text-shadow:_1px_1px_0px_#000000ab]">
+            Max Franklin
+          </h1>
+        </Link>
+        <Navlist />
+      </div>
     </header>
   );
 };
