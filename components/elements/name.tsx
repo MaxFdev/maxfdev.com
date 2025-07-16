42e95cad8eeb9debda9f26c2247810e055da5e48
Max F <maxfranklin@maxfdev.com>
2025-07-16 00:45:21 -0400

    update projects beta

--- components/elements/name.tsx
+++ components/elements/name.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Name = () => {
+  const pathname = usePathname();
+
   return (
     <Link
-      href="/#"
+      href="/"
       onClick={(e) => {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: "smooth" });
+        if (pathname === "/") {
+          e.preventDefault();
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }
       }}
     >
       <h1 className="font-verdana text-blue-500 font-semibold text-3xl [text-shadow:_1px_1px_0px_#000000ab]">
--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectCarousel from "@/components/elements/projectCarousel";
 import BigButton from "@/components/elements/bigButton";
+import Link from "next/link";
 
 // TODO finish and fix styling
 // TODO make projects come from github
@@ -12,6 +13,16 @@ const projects = () => {
       className="flex flex-col justify-center items-center gap-[var(--gap-clamp)]"
     >
       <h2 className="text-center">Some Of My Projects.</h2>
+      {/* HACK remove this when the dynamic version is in place */}
+      <div className="-my-8 text-muted-foreground">
+        Check out the beta for the dynamic version{" "}
+        <Link
+          className="text-blue-500"
+          href="/projectstest"
+        >
+          HERE
+        </Link>
+      </div>
       <ProjectCarousel />
       <BigButton
         href="https://github.com/MaxFdev"
