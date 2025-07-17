cd4f1f29d39b18fdf16dc83c11e1464faef4904e
Max F <maxfranklin@maxfdev.com>
2025-07-16 23:54:28 -0400

    update contact

--- components/elements/contact.tsx
+++ components/elements/contact.tsx
@@ -9,9 +9,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
-// TODO change icons
-// TODO finalize styling
+import { ArrowUpRight } from "lucide-react";
 
 const Contact = () => {
   return (
@@ -22,28 +20,38 @@ const Contact = () => {
       <DialogContent className="max-sm:w-96 max-sm:rounded-lg">
         <DialogHeader>
           <DialogTitle asChild>
-            <h3>Contact & Socials</h3>
+            <h3>Contact me</h3>
           </DialogTitle>
           <DialogDescription asChild>
-            <p className="text-blue-300">Here are all my accounts.</p>
+            <p className="text-blue-300">Here are my accounts.</p>
           </DialogDescription>
         </DialogHeader>
-        <ul>
+        <ul className="flex flex-col gap-1">
           {socials.map((platform, key) => (
             <li
               key={key}
               className="flex justify-between items-center font-semibold w-full border-b-[1px]"
             >
-              <div className="flex flex-col justify-center items-start">
+              <div className="flex justify-center gap-1">
+                {platform.icon && (
+                  <platform.icon
+                    size={28}
+                    className="border border-black rounded-full p-1 overflow-visible"
+                  />
+                )}
                 <h4>{platform.title}:</h4>
-                <p className="text-blue-500">{platform.user}</p>
+                <span className="text-blue-500">{platform.user}</span>
               </div>
-              <Button
-                href={platform.link}
-                target="_blank"
-              >
-                Go To
-              </Button>
+              <div className="p-1">
+                <Button
+                  href={platform.link}
+                  target="_blank"
+                  className="flex gap-1 items-center"
+                >
+                  Go To
+                  <ArrowUpRight size={20} />
+                </Button>
+              </div>
             </li>
           ))}
         </ul>
--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,5 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
 export const intro = {
   text: "Hello, I'm Max, a technology enthusiast pursuing a Bachelor's degree in Computer Science at Yeshiva University. My studies are focussed in Distributed Systems, and my goal is to work in the Software Engineering industry developing cutting edge systems. I have a lot of experience with developing systems and projects in Java, as well building full-stack websites.",
 };
@@ -38,16 +40,19 @@ export const projectItems = [
 export const socials = [
   {
     title: "Email",
+    icon: Mail,
     user: "maxfranklin@maxfdev.com",
     link: "mailto:maxfranklin@maxfdev.com",
   },
   {
     title: "GitHub",
+    icon: Github,
     user: "MaxFdev",
     link: "https://github.com/MaxFdev",
   },
   {
     title: "LinkedIn",
+    icon: Linkedin,
     user: "Max Franklin",
     link: "https://www.linkedin.com/in/max--franklin/",
   },
