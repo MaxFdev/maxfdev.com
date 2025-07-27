fe3d62ed8ac28138d898328a25f4795f3106c5f0
MaxFdev <maxfranklin@maxfdev.com>
2025-07-27 16:23:42 -0400

    bug testing

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -6,5 +6,6 @@ export const runtime = "edge";
 export async function GET() {
   const username = process.env.NEXT_PUBLIC_GITHUB_ACCOUNT;
   const projects = await fetchProjectDetails(username || "");
+  if (projects.length === 0) return NextResponse.json({"Error": "it broke."});
   return NextResponse.json(projects);
 }
