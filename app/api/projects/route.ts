bbf09beb83444529b2db6741158664eec29b8f41
MaxFdev <maxfranklin@maxfdev.com>
2025-07-27 15:58:06 -0400

    update api

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { fetchProjectDetails } from "@/utils/projects";
 
+export const runtime = "edge";
+
 export async function GET() {
   const username = process.env.NEXT_PUBLIC_GITHUB_ACCOUNT;
   const projects = await fetchProjectDetails(username || "");
