9ade57d9a2c5936e3a1b1f7af3f543635bb54925
MaxFdev <maxfranklin@maxfdev.com>
2025-07-27 16:35:06 -0400

    update env variable handling

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 import { fetchProjectDetails } from "@/utils/projects";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 
 export const runtime = "edge";
 
 export async function GET() {
-  const username = process.env.NEXT_PUBLIC_GITHUB_ACCOUNT;
-  const projects = await fetchProjectDetails(username || "");
-  if (projects.length === 0) return NextResponse.json({"Error": "it broke."});
+  const username = getRequestContext().env.NEXT_PUBLIC_GITHUB_ACCOUNT;
+  if (!username) return NextResponse.json({ error: "Missing username env variable" });
+  const projects = await fetchProjectDetails(username);
+  if (projects.length === 0) return NextResponse.json({ error: "No projects found." });
   return NextResponse.json(projects);
 }
