adfa35c4a712734b6556f5c37a69e08063acfe61
MaxFdev <maxfranklin@maxfdev.com>
2025-07-27 17:03:22 -0400

    update to static api variables

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -4,9 +4,7 @@ import { fetchProjectDetails } from "@/utils/projects";
 export const runtime = "edge";
 
 export async function GET() {
-  const username = process.env.GITHUB_ACCOUNT;
-  if (!username) return NextResponse.json({ error: "Missing username env variable" });
+  const username = "MaxFdev";
   const projects = await fetchProjectDetails(username);
-  if (projects.length === 0) return NextResponse.json({ error: "No projects found." });
   return NextResponse.json(projects);
 }
