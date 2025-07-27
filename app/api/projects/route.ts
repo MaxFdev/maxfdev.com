b2a05cca9b7259d6c8f4234da453d5bb131e95ed
MaxFdev <maxfranklin@maxfdev.com>
2025-07-27 16:40:44 -0400

    undo last

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 import { fetchProjectDetails } from "@/utils/projects";
-import { getRequestContext } from "@cloudflare/next-on-pages";
 
 export const runtime = "edge";
 
 export async function GET() {
-  const username = getRequestContext().env.NEXT_PUBLIC_GITHUB_ACCOUNT;
+  const username = process.env.NEXT_PUBLIC_GITHUB_ACCOUNT;
   if (!username) return NextResponse.json({ error: "Missing username env variable" });
   const projects = await fetchProjectDetails(username);
   if (projects.length === 0) return NextResponse.json({ error: "No projects found." });
