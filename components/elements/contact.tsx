302e10d9d8d5833e41de59fa0ff0262c0e5a2e84
Max F <meirmaxf11@gmail.com>
2026-02-25 00:11:18 -0500

    fix contact dialog

--- components/elements/contact.tsx
+++ components/elements/contact.tsx
@@ -18,11 +18,11 @@ const Contact = () => {
       </DialogTrigger>
       <DialogContent
         slideFrom="top-right"
-        className="max-sm:w-96 max-sm:rounded-lg"
+        className="w-19/20 max-w-4xl rounded-2xl! max-h-[85vh] overflow-y-auto"
       >
         <DialogHeader>
           <DialogTitle asChild>
-            <h3>Contact me</h3>
+            <h3>Contact Info</h3>
           </DialogTitle>
           <DialogDescription asChild>
             <p className="text-blue-300">Here are my accounts.</p>
@@ -32,9 +32,9 @@ const Contact = () => {
           {socials.map((platform, key) => (
             <li
               key={key}
-              className="flex justify-between items-center font-semibold w-full border-b"
+              className="flex justify-between items-end sm:items-center font-semibold w-full border-b p-1"
             >
-              <div className="flex justify-center gap-1">
+              <div className="flex flex-col sm:flex-row justify-center sm:gap-1">
                 {platform.icon && (
                   <platform.icon
                     size={28}
@@ -44,16 +44,14 @@ const Contact = () => {
                 <h4>{platform.title}:</h4>
                 <span className="text-blue-500">{platform.user}</span>
               </div>
-              <div className="p-1">
-                <Button
-                  href={platform.link}
-                  target="_blank"
-                  className="flex gap-1 items-center"
-                >
-                  Go To
-                  <ArrowUpRight size={20} />
-                </Button>
-              </div>
+              <Button
+                href={platform.link}
+                target="_blank"
+                className="flex gap-1 items-center text-nowrap"
+              >
+                Go To
+                <ArrowUpRight size={20} />
+              </Button>
             </li>
           ))}
         </ul>
